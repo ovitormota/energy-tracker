18620c4114fa680860dc7213599bf30f5fe9a1f1
vitor.goncalves <vitormotagoncalvess@gmail.com>
2025-04-06 23:24:17 -0300

    feat: initializing frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { CssBaseline } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
